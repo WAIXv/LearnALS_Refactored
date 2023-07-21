ae4e7f7e63f2476cb396f95c0ccbd2f2a30dc19d
WAI <why18520259443@gmail.com>
2023-07-21 16:30:31 +0800

    重新开始！决定了就复刻原版蓝图，专注了解als这个动画系统案例，c++、重构什么的有需求再学

---
+++ Plugins/ALS-Refactored-4.10/Source/ALS/Public/AlsAnimationInstance.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "AlsAnimationInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALS_API UAlsAnimationInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+};
